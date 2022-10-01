28bd398fce15faf40ca2b340612dc1b448151321
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2022-10-01 19:12:08 -0400

    now gets and solves problems accurately

--- math_class.py
+++ math_class.py
@@ -1,15 +1,84 @@
 import socket
+import re
+import statistics
 
 HOST = '143.110.215.221'
 PORT = 10000
 
-def main():
-    connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def init():
+    # initialize the socket
+    global connection
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.connect((HOST, PORT))
+    # skip first packet (header and welcome message)
+    _ = connection.recv(PORT)
+
+
+def get_problem_type(data):
+    '''
+    returns what type of problem we have (range, minimum, maximum, etc)
+    '''
+    # check for problem type and return it
+    if re.match(".*range.*", str(data)):
+        return "range"
+    elif re.match(".*minimum.*", str(data)):
+        return "minimum"
+    elif re.match(".*maximum.*", str(data)):
+        return "maximum"
+    elif re.match(".*mean.*", str(data)):
+        return "mean"
+    elif re.match(".*median.*", str(data)):
+        return "median"
+    # return None if we don't know what the problem is
+    else:
+        return None
+
+
+def sanitize_data(data):
+    '''
+    sanitizes data from server so we can just extract the numbers
+    '''
+    # tokenize data after converting to string. Not sure why I need to use \\n instead of just \n
+    tokens = str(data).split("\\n")
+    # remove first 2 tokens (some random letter b and problem info) and the last one (random ')
+    del tokens[0:2]
+    del tokens[-1]
+    return tokens
+
+
+def solve(tokens, problem_type):
+    '''
+    solves problem, given numbers as tokens and problem_type
+    '''
+    # convert tokens to integers
+    numbers = [int(x) for x in tokens]
+    match problem_type:
+        case "range":
+            return max(numbers)-min(numbers)
+        case "minimum":
+            return min(numbers)
+        case "maximum":
+            return max(numbers)
+        case "mean":
+            return sum(numbers)/len(numbers)
+        case "median":
+            return statistics.median(numbers)
+        case _:
+            return None
+
+
+
+
+def main():
+    # initialize connection
+    init()
+    # get first problem
     data = connection.recv(PORT)
-    print(data)
-    data = connection.recv(PORT)
-    print(data)
+    solution = solve(sanitize_data(data), get_problem_type(data))
+    print(solution)
+    connection.close()
 
 
 if __name__ == "__main__":
