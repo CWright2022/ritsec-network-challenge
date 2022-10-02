646b7fb7fff5bc4deb3b2583456aec591dd1ac67
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2022-10-02 16:28:21 -0400

    solved recieve problem. It works!

--- math_class.py
+++ math_class.py
@@ -12,23 +12,25 @@ def init():
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.connect((HOST, PORT))
     # skip first packet (header and welcome message)
-    _ = connection.recv(PORT)
+    connection.recv(1996)
+    connection.settimeout(0.5)
 
 
-def get_problem_type(data):
+def get_problem_type(sanitized_data):
     '''
     returns what type of problem we have (range, minimum, maximum, etc)
     '''
+    problem_string=sanitized_data.pop(0)
     # check for problem type and return it
-    if re.match(".*range.*", str(data)):
+    if re.match(".*range.*", str(problem_string)):
         return "range"
-    elif re.match(".*minimum.*", str(data)):
+    elif re.match(".*minimum.*", str(problem_string)):
         return "minimum"
-    elif re.match(".*maximum.*", str(data)):
+    elif re.match(".*maximum.*", str(problem_string)):
         return "maximum"
-    elif re.match(".*mean.*", str(data)):
+    elif re.match(".*mean.*", str(problem_string)):
         return "mean"
-    elif re.match(".*median.*", str(data)):
+    elif re.match(".*median.*", str(problem_string)):
         return "median"
     # return None if we don't know what the problem is
     else:
@@ -40,10 +42,10 @@ def sanitize_data(data):
     sanitizes data from server so we can just extract the numbers
     '''
     # tokenize data after converting to string. Not sure why I need to use \\n instead of just \n
-    tokens = str(data).split("\\n")
-    # remove first 2 tokens (some random letter b and problem info) and the last one (random ')
-    del tokens[0:2]
-    del tokens[-1]
+    tokens = str(data).split("\n")
+    # remove first and last token
+    del(tokens[0])
+    del(tokens[-1])
     return tokens
 
 
@@ -52,36 +54,60 @@ def solve(tokens, problem_type):
     solves problem, given numbers as tokens and problem_type
     '''
     # convert tokens to integers
-    try:
-        numbers = [int(x) for x in tokens]
-    except:
-        print("Error converting tokens to integers - try again in like 5s")
-        return None
+    numbers = [int(x) for x in tokens]
     match problem_type:
         case "range":
-            return max(numbers)-min(numbers)
+            return max(numbers) - min(numbers)
         case "minimum":
             return min(numbers)
         case "maximum":
             return max(numbers)
         case "mean":
-            return sum(numbers)/len(numbers)//1
+            return int(round(statistics.mean(numbers)))
         case "median":
-            return statistics.median(numbers)//1
+            return int(round(statistics.median(numbers)))
         case _:
             return None
-
+def recieve_all():
+    '''
+    recieve all data (stolen from alex B, the best man ever)
+    '''
+    received = b''
+    while True:
+        try:
+            received += connection.recv(4096)
+            if re.match(".*RS{.*}.*", str(received)):
+                break
+        except:
+            break
+    return received.decode()
 
 def main():
     # initialize connection
     init()
-    # get first problem
-    data = connection.recv(PORT)
-    print("Problem:", get_problem_type(data), "of", sanitize_data(data))
-    print("Answer:", solve(sanitize_data(data), get_problem_type(data)))
-    solution = solve(sanitize_data(data), get_problem_type(data))
-    print(solution)
-    connection.shutdown(socket.SHUT_RDWR)
+    while True:
+        #recieve data
+        data = recieve_all()
+        print("Raw data: ", data)
+        #sanitize data
+        sanitized_data = sanitize_data(data)
+        print("sanitized data: ", sanitized_data)
+        #get problem type
+        problem_type = get_problem_type(sanitized_data)
+        print("Problem:", problem_type, "of", sanitized_data)
+        #solve problem
+        solution = solve(sanitized_data, problem_type)
+        print("Solution:", solution)
+        # send solution
+        print("Sending solution:", solution)
+        message=str(solution)+"\n"
+        connection.sendall(message.encode())
+        #if we got the flag, then break
+        if re.match(".*RS{.*}", str(data)):
+            print("Flag:", data)
+            #shutdown connection
+            connection.shutdown(socket.SHUT_RDWR)
+            break
 
 
 if __name__ == "__main__":
